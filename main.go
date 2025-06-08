2905db53f91c08f31d6d8c4016a731e73d710f65
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-06-08 18:04:15 +0800

    Update: Added guess

--- main.go
+++ main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"math/rand"
 	"strconv"
 
 	"github.com/pterm/pterm"
 )
 
-type Input struct {
-	numberInput int
-}
-
 func main() {
 	pterm.Info.Println("Input your Number")
-	input, _ := pterm.DefaultInteractiveTextInput.Show("Enter you Guess Number")
-	newInput, _ := strconv.ParseInt(input, 0, 64)
 
-	trackInput(newInput)
+	gNumber := randomNumber()
+	inpNumber := 5
+
+	for {
+		input, _ := pterm.DefaultInteractiveTextInput.Show("Enter you Guess Number")
+		newInput, _ := strconv.Atoi(input)
+		if inpNumber != 0 {
+			npNumber := trackInput(newInput, gNumber, inpNumber)
+			pterm.Info.Println("You have ", npNumber, " guesses left")
+		}
+
+	}
 }
 
 // track the user input
-func trackInput(input int64) {
+func trackInput(input int, gNumber int, npNumber int) int {
+	if input > gNumber {
+		pterm.Warning.Println("The number is less than the Input")
+		npNumber -= 1
+	} else if input < gNumber {
+		pterm.Warning.Println("The number is greater than the input")
+		npNumber -= 1
+	} else if input == gNumber {
+		pterm.Success.Println("You guess it right")
+	}
+
+	return npNumber
+}
+
+// select random number
+func randomNumber() int {
+	ranNumber := rand.Intn(100)
+
+	return ranNumber
 }
